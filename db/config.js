485764193805b4d685e94b88c68009540fb9e1a7
kst <zyzuzizozz@naver.com>
2022-03-30 17:54:32 +0900

    mysql패키지설치및DB연동셋팅

---
+++ db/config.js
@@ -0,0 +1,11 @@
+ 
+
+module.exports = {
+
+    host : "myfirstdb-1.cqzp0xslvjuo.us-east-1.rds.amazonaws.com",
+    user : "admin",
+    password : "zyzuzizozp1!",
+    port : "3306",
+    database : "AAA",
+}
+
